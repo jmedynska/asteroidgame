cf0cbbc4271742016008e8ae1e2d5c504f6ec7bd
Julia Medyńska <julia.medynska1@gmail.com>
2023-01-17 10:57:54 +0100

    Add basic code

--- asteroids.py
+++ asteroids.py
@@ -1,2 +1,16 @@
 import pygame
 from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((800,800))
+pygame.display.set_caption("Asteroids")
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+    screen.fill((0,0,0))
+    pygame.display.update()
+
+pygame.quit()
